af8dd83aadc10efd6d481ea0588f627476fcd952
Oliver <oc@hejverden.dk>
2017-09-04 20:15:42 +0200

    Add solution

---
+++ substrings.rb
@@ -0,0 +1,23 @@
+# Return a hash listing each substring (case insensitive) that was found in
+# the original string and how many times it was found.
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(sentence, dictionary)
+  result = Hash.new
+  dictionary.each do |entry|
+    # Downcase the sentence and find each word
+    sentence.downcase.scan(/\w+/).each do |word|
+      if word.include?(entry)
+        # If the word include an entry from the directory make a key-value pair or increment existing value
+        result[entry].nil? ? result[entry] = 1 : result[entry] += 1
+      end
+    end
+  end
+  # Return hash with sorted results by descending values
+  result.sort_by {|_key, value| value}.reverse.to_h
+end
+
+
+# substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => {"i"=>3, "it"=>2, "how"=>2, "own"=>1, "howdy"=>1, "sit"=>1, "partner"=>1, "part"=>1, "going"=>1, "go"=>1, "down"=>1}
