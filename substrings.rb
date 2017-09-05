15944f99c8f65c30a05566dc1fd4931b25a20278
Oliver <oc@hejverden.dk>
2017-09-05 16:03:43 +0200

    Update code with a hash with default value. Cleaner code.

--- substrings.rb
+++ substrings.rb
@@ -4,13 +4,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(sentence, dictionary)
-  result = Hash.new
+  result = Hash.new(0)
   dictionary.each do |entry|
     # Downcase the sentence and find each word
     sentence.downcase.scan(/\w+/).each do |word|
       if word.include?(entry)
         # If the word include an entry from the directory make a key-value pair or increment existing value
-        result[entry].nil? ? result[entry] = 1 : result[entry] += 1
+        result[entry] += 1
       end
     end
   end
